73617421fc45e746054d56b3732216029cbeba9e
morrison-turnansky <mturnans@redhat.com>
2026-06-26 11:55:28 +0000

    trying FakeTensorObservedException approach

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -53,7 +53,12 @@ from typing_extensions import TypeIs
 import torch
 import torch._logging
 from torch._dynamo.dynamo_profiler import DynamoProfilerState, FunctionTraceTiming
-from torch._dynamo.exc import ObservedException, TensorifyScalarRestartAnalysis
+from torch._dynamo.exc import (
+    get_dynamo_observed_exception,
+    FakeTensorObservedException,
+    ObservedException,
+    TensorifyScalarRestartAnalysis,
+)
 from torch._guards import InlinedCodeCache, tracing, TracingContext
 from torch._logging.structured import dump_file
 from torch.fx.experimental.symbolic_shapes import guard_bool
@@ -1687,6 +1692,24 @@ class InstructionTranslatorBase(
             return not self.output.should_exit
         except TensorifyScalarRestartAnalysis:
             raise
+        except FakeTensorObservedException as e:
+            try:
+                self.exception_handler(e)
+            except Unsupported:
+                curr = self.exn_vt_stack.get_current_exception()
+                msg = ""
+                if isinstance(curr, variables.ExceptionVariable) and curr.args:
+                    msg = curr.args[0].as_python_constant()
+                formatted = exc.format_graph_break_message(
+                    gb_type="RuntimeError when making fake tensor call",
+                    context="",
+                    explanation=msg,
+                    hints=[*graph_break_hints.USER_ERROR],
+                )
+                raise exc.TorchRuntimeError(formatted, e.real_stack) from None
+            if e.node is not None:
+                self.output.graph.erase_node(e.node)
+            return True
         except exc.ObservedException as e:
             self.exception_handler(e)
             return True
@@ -2803,7 +2826,9 @@ class InstructionTranslatorBase(
             # Bubble the exception to the interpreter
             curr_exc = self.exn_vt_stack.get_current_exception()
             dynamo_exc = exc.get_dynamo_observed_exception(curr_exc.python_type())
-            if not isinstance(raised_exception, dynamo_exc):
+            if not isinstance(
+                raised_exception, (dynamo_exc, FakeTensorObservedException)
+            ):
                 raise AssertionError(
                     "expected isinstance(raised_exception, dynamo_exc) to be true"
                 )  # sanity check
