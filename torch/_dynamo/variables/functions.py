01ffea793912c14055af5ad188183f5b25c3c0fc
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2026-07-14 10:36:07 +0000

    Fix some tests.

--- torch/_dynamo/variables/functions.py
+++ torch/_dynamo/variables/functions.py
@@ -2877,7 +2877,7 @@ class PolyfilledFunctionVariable(VariableTracker):
                     *[x.as_python_constant() for x in args],
                     **{k: v.as_python_constant() for k, v in kwargs.items()},
                 )
-            except (TypeError, ValueError) as exc:
+            except Exception as exc:
                 raise_observed_exception(
                     type(exc),
                     tx,
